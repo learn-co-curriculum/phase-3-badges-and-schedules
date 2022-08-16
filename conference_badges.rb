4c24eefbfc545a39a8c3359397e8d4c626f9a424
korosdennis <deniptoo98@gmaiil.com>
2022-08-16 20:10:57 +0300

    commit tree 24cb06a94b22e0f722b9c79d463024a90db2c1ec

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+def batch_badge_creator(array)
+    new_array=[]
+    array.each do |elem|
+        new_array.push("Hello, my name is #{elem}.")
+    end
+    return new_array
+end
+
+def assign_rooms(array)
+    room = []
+    array.each.with_index do |name,index| 
+        room.push("Hello, #{name}! You'll be assigned to room #{index+1}!") 
+    end
+    return room
+end
+
+def printer(array)
+    batch_badge_creator(array).each{|elem| puts elem}
+    assign_rooms(array).each{|elem| puts elem}
+end
